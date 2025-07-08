077e908e55c80eeac0198baf4306734a10b63caa
Eduard Bohn <eduard.bohn@mail.de>
2025-07-08 20:50:07 +0200

    camelize: convert kebab-case to camelCase

---
+++ script.js
@@ -0,0 +1,15 @@
+//Translate border-left-width to borderLeftWidth
+
+function camelize(str) {
+    let words = str.split("-");
+    words.map((word, index) => {
+        if (!(word === words[0])) {
+            words[index] = word.at(0).toUpperCase().concat(word.slice(1));
+        }
+    });
+    return words.join("");
+}
+
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
